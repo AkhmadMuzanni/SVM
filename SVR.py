8f236e60bef046bd3c04f2c00f8a0ad988d9e26d
Akhmad Muzanni Safi'i <akhmadmuzannisafii@gmail.com>
2018-08-11 22:17:35 +0700

    add method for read csv file

--- SVR.py
+++ SVR.py
@@ -2,6 +2,18 @@
 """
 Created on Sat Aug 11 21:48:34 2018
 
-@author: USER
+@author: MUZANNI
 """
 
+import csv
+def read_csv(file_name):
+    array_2D = []
+    with open(file_name, 'rb') as csvfile:
+        read = csv.reader(csvfile, delimiter=';')
+        for row in read:
+            array_2D.append(row)
+    return array_2D
+
+dataTraining = read_csv("dataTraining.csv")
+for data in dataTraining:
+    print(data)
