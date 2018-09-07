8becb5e93b4d3789425a1dee1b4b7113a9402749
Akhmad Muzanni Safi'i <akhmadmuzannisafii@gmail.com>
2018-09-07 15:32:43 +0700

    Fix: Testing with form

--- frontend.php
+++ frontend.php
@@ -112,26 +112,32 @@ $cmd = "$python $pyscript";
 					<div class="modal-content">
 						<div class="modal-header">
 							<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-							<h4 class="modal-title">ESTIMASI PRODUKSI PADI</h4>
+							<h4 class="modal-title">ESTIMASI PRODUKSI</h4>							
 						</div>
 						<div class="modal-body">
-							<form class="form-horizontal">
+							<form class="form-horizontal" method="post" action="testing.php">
+								<div class="form-group">
+									<label class="control-label col-sm-4">Komoditas:</label>
+									<div class="col-sm-8">
+										<input type="text" class="form-control" id="komoditas" value="PADI" disabled="true" name="komoditas">
+									</div>
+								</div>
 								<div class="form-group">
 									<label class="control-label col-sm-4">Luas Tanam:</label>
 									<div class="col-sm-8">
-										<input type="number" class="form-control" placeholder="Luas Tanam" id="tanam_padi">
+										<input type="number" class="form-control" placeholder="Luas Tanam" id="tanam_padi" name="luas_tanam">
 									</div>
 								</div>
 								<div class="form-group">
 									<label class="control-label col-sm-4">Jumlah Penduduk:</label>
 									<div class="col-sm-8">
-										<input type="number" class="form-control" placeholder="Jumlah Penduduk" id="penduduk">
+										<input type="number" class="form-control" placeholder="Jumlah Penduduk" id="penduduk" name="jml_penduduk">
 									</div>
 								</div>
 								<div class="form-group">
 									<label class="control-label col-sm-4">Luas Lahan (Sawah):</label>
 									<div class="col-sm-8">
-										<input type="number" class="form-control" placeholder="Luas Lahan (Sawah)" id="luas_lahan">
+										<input type="number" class="form-control" placeholder="Luas Lahan (Sawah)" id="luas_lahan" name="luas_lahan">
 									</div>
 								</div>
 								<div class="form-group">
--- testing.php
+++ testing.php
@@ -1,5 +1,9 @@
-<?php 
-	require_once('db.php');
+<?php 	
+	require_once('db.php');		
+	$sigma = 0.3;
+	$lambda = 0.1;
+	$max_value = 2576596;
+	$min_value = 45888;
 	$alpha = array(
 		14.09446540328213, 
 		-14.042057438070751, 
@@ -12,13 +16,13 @@
 		-73.9223889057608, 
 		11.98775314182274, 
 		3.2600614453296224);
-	print_r($alpha);
+	// print_r($alpha);	
 
-	echo "<br>";
+	// echo "<br>";
 	
 
 	$sql="SELECT luas_tanam,jml_penduduk,luas_sawah FROM padi";
-	$data_training = array();
+	$data_training = array();	
 
 	if ($result=mysqli_query($link,$sql)){	  
 		while ($row=mysqli_fetch_row($result)){
@@ -26,8 +30,61 @@
 		}
 	}
 
-	print_r($data_training);
+	// $data_test = array(70289,2544315,45888);
+	$data_test = array($_POST["luas_tanam"],$_POST["jml_penduduk"],$_POST["luas_lahan"]);
 
-	mysqli_close($link)
+	$norm_test = array();
+	foreach ($data_test as $test) {
+		array_push($norm_test, ($test - $min_value) / ($max_value - $min_value));
+	}
+	print_r($norm_test);
+
+	// add data testing
+	// $data_test = array(0.009641966, 0.987244281, 0);		
+	// print_r($data_test);	
+
+	mysqli_close($link);
+	
+	// count distance for data testing
+	// echo "jarak";
+	$jarak_test = array();
+	for ($i=0; $i < count($data_training) ; $i++) { 
+		$jarak_test[$i] = 0;
+		for ($j=0; $j < count($data_training[0]); $j++) {
+			$jarak_test[$i] += pow(($data_training[$i][$j] - $data_test[$j]), 2);
+		}
+	}
+
+	// count kernel for data testing
+	$kernel_test = array();
+	for($i=0 ; $i < count($jarak_test) ; $i++){
+		$kernel_test[$i] = exp(-($jarak_test[$i]/(2*pow($sigma,2))));
+	}
+
+	// count hessian for data testing
+	$hessian_test = array();
+	for($i=0 ; $i < count($kernel_test) ; $i++){
+		$hessian_test[$i] = $kernel_test[$i]+pow($lambda,2);
+	}
+
+	// count estimation
+	$estimasi = 0;
+	for($i=0 ; $i < count($hessian_test) ; $i++){
+		$estimasi += $alpha[$i]*($hessian_test[$i]+pow($lambda,2));
+	}
+
+	$estimasi_denorm = ($estimasi*($max_value - $min_value)) + $min_value;
+
+	// echo "<br>";
+	echo "Luas Tanam = ".$_POST["luas_tanam"]."<br>";
+	echo "Jumlah Penduduk = ".$_POST["jml_penduduk"]."<br>";
+	echo "Luas Lahan (Sawah) = ".$_POST["luas_lahan"]."<br>";
+	echo "HASIL ESTIMASI = ".$estimasi_denorm;
+	echo "<br>";
+
+	// echo $_POST["luas_tanam"];
+
+
+
 
 ?>
